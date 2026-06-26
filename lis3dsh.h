d75ab2b58b5778e7e00e42c110822c156a327234
Charlie <charlestejano8@gmail.com>
2026-06-26 18:05:48 -0500

    Added accelerometer prototypes and helper

--- lis3dsh.h
+++ lis3dsh.h
@@ -7,7 +7,21 @@
 #include <stdint.h>
 #include "stm32f4xx.h"
 
+// g-direction helper
+typedef enum {
+    DIR_NONE = 0,
+    DIR_POSITIVE_X,
+    DIR_NEGATIVE_X,
+    DIR_POSITIVE_Y,
+    DIR_NEGATIVE_Y,
+    DIR_POSITIVE_Z,
+    DIR_NEGATIVE_Z
+} LIS3DSH_Direction;
+
+LIS3DSH_Direction LIS3DSH_Dir(void); // Returns the direction of the g-force detected from the acceerometer
+
 // Prototypes
 void LIS3DSH_Init(void);
+void LIS3DSH_Read(float *x_dir, float *y_dir, float *z_dir);
 
 #endif // LIS3DSH_H

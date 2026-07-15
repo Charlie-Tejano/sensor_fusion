ce87cce26414f10a01ab8bfa05e0e311b9a378ec
Charlie <charlestejano8@gmail.com>
2026-07-15 17:23:36 -0500

    Compact motor and ultrasonic into while(1){ } loop

--- main.c
+++ main.c
@@ -44,15 +44,23 @@ int main(void)
 {
     LCD_port_init();
     LCD_init();
-    MOTOR_Init();
-		ultrasonicInit();
+    lis3dsh_init();
 
     // Debug mode for SPI communication with LIS3DSH
     #if SPI_DEBUG
     LCD_placeCursor(1);
     LCD_printString("SPI Debug Mode...");
 
+    #else
+    MOTOR_Init();
+    ultrasonicInit();
+    MOTOR_Stop();
+    MOTOR_SetDutyCycle(40u);
+    MOTOR_Forward();
+    #endif
+
     while(1) {
+    #if SPI_DEBUG
         float x_dir, y_dir, z_dir;
         char dbg[17];
 
@@ -62,42 +70,18 @@ int main(void)
         LCD_placeCursor(2);
         LCD_printString(dbg);
         delay_ms(100); // Update every 100 ms
-    }
-    #endif
-
-    while(1) {
-        uint8_t id = lis3dsh_whoami(); // Read WHO_AM_I register from LIS3DSH
-        char dbg[17];
-
-        snprintf(dbg, sizeof(dbg), "WHO_AM_I: 0x%02X", id); // Display WHO_AM_I value on LCD
-        LCD_placeCursor(1);
-        LCD_printString(dbg);
-        delay_ms(100); // Update every 100 ms
-    }
+        #else
     
-    MOTOR_Stop(); // Stop motor right away
-
-    LCD_placeCursor(1);
-    LCD_printString("Digital Sensor Fusion ");
-    LCD_placeCursor(2);
-    LCD_printString("Initializing... ");
-    delay_ms(1000);
-
-    // Start forward at 40% as our certain speed
-    MOTOR_SetDutyCycle(40u);
-    MOTOR_Forward();
-
-    while (1) {
-        float dist_cm = ultrasonic_GetDistance_cm(); // Get distance from ultrasonic sensor
-
+        float dist_cm = ultrasonic_GetDistance_cm();
         if (dist_cm <= 0.0f) { // Less than 0 cm is not valid
             delay_ms(50);
             continue;
         }
 
         uint8_t duty = DIST_to_duty(dist_cm);
-        MOTOR_SetDutyCycle(duty); // Set motor duty cycle based on distance
-        LCD_showUltrasonic(dist_cm, duty); // Show distance and duty cycle on LCD
+        MOTOR_SetDutyCycle(duty);
+        LCD_showUltrasonic(dist_cm, duty);
         delay_ms(100);
+        #endif
     }
 }

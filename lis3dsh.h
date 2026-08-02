7f469c48c2ed19352ffd11ac5d2735760de725fa
Charlie <charlestejano8@gmail.com>
2026-08-01 19:25:33 -0500

    Added tilt and shake detection for the lis3dsh

--- lis3dsh.h
+++ lis3dsh.h
@@ -18,7 +18,32 @@ typedef enum {
     DIR_NEGATIVE_Z
 } LIS3DSH_Direction;
 
-LIS3DSH_Direction lis3dsh_dir(void); // Returns the direction of the g-force detected from the accelerometer
+// Returns the direction of the g-force detected from the accelerometer
+LIS3DSH_Direction lis3dsh_dir(void);
+
+// Tilt, impact, and shake detection
+typedef enum {
+    detect_none = 0,
+    detect_tilt,
+    detect_impact,
+    detect_shake
+} tilt_detect_t;
+
+tilt_detect_t lis3dsh_detect(void); // Detects tilt
+
+// impact and shake detection
+typedef enum {
+    impact_none = 0,
+    impact_positive_x,
+    impact_negative_x,
+    impact_positive_y,
+    impact_negative_y,
+    impact_positive_z,
+    impact_negative_z
+} impact_detect_t;
+
+// Detects impact when shaken
+impact_detect_t lis3dsh_impact(void);
 
 // Prototypes
 void lis3dsh_init(void);
--- ultrasonic.h
+++ ultrasonic.h
@@ -7,9 +7,11 @@
 #include "stm32f4xx.h"
 #include <stdint.h>
 
+// Function prototypes
 void  ultrasonicInit(void);
 float ultrasonic_GetDistance_cm(void);
 
+// Borrow the global variable from ultrasonic.c to be used in main.c
 extern volatile float g_distance_cm;
 
 #endif
